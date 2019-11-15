e3a98f8e34e38bc4109451d5f79e3ee20ee3388f
omairazam <omsolutionpk@gmail.com>
2019-11-15 16:45:56 +0500

    helper methods imported from spree i18n - quick hack

--- app/helpers/spree_globalize/locale_helper.rb
+++ app/helpers/spree_globalize/locale_helper.rb
@@ -16,12 +16,34 @@ module SpreeGlobalize
       )
     end
 
+    def select_available_locales
+      select_tag('available_locales[]', options_for_select(
+        all_locales_options,
+        Config.available_locales
+      ), common_options)
+    end
+
+
+    # Need to manually add en to the array because the en.yml was moved from
+    # this project. spree/spree now has those keys.
+    def all_locales_options
+      SpreeI18n::Locale.all.map { |locale| self.locale_presentation(locale) }.push(['English (EN)', :en])
+    end
+
+    def available_locales_options
+      Config.available_locales.map { |locale| self.locale_presentation(locale) }
+    end
+
     def locale_presentation(locale)
       [Spree.t(:'i18n.this_file_language', locale: locale), locale]
     end
     
     def supported_locales_options
       Config.supported_locales.map { |locale| locale_presentation(locale) }
+    end
+
+    def common_options
+      { class: 'fullwidth', multiple: 'true' }
     end
   end
 end
